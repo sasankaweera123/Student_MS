447498261189e5fe351934aa6f6a25714a62142e
sasankaweera123 <sajewsasanka1@gmail.com>
2023-07-01 08:00:50 +0530

    Update app.component.ts
    - Filter Student Function
    - Save Student Function

--- student_ms_app/src/app/app.component.ts
+++ student_ms_app/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { StudentService } from './service/student.service';
-import { Observable, catchError, map, of, startWith } from 'rxjs';
+import {
+  BehaviorSubject,
+  Observable,
+  catchError,
+  map,
+  of,
+  startWith,
+} from 'rxjs';
 import { AppSatate } from './interface/app-state';
 import { CustomResponse } from './interface/custom-response';
 import { DataState } from './enum/dataState';
 import { Gender } from './enum/gender.enum';
+import { NgForm } from '@angular/forms';
+import { Student } from './interface/student';
 
 @Component({
   selector: 'app-root',
@@ -15,6 +24,8 @@ export class AppComponent implements OnInit {
   appState$: Observable<AppSatate<CustomResponse>>;
   readonly DataState = DataState;
   readonly Gender = Gender;
+  private dataSubject = new BehaviorSubject<CustomResponse | null>(null);
+  selectedGender: Gender = Gender.ALL;
 
   constructor(private studentService: StudentService) {
     this.appState$ = of({ dataState: DataState.LOADING_STATE });
@@ -23,11 +34,57 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.appState$ = this.studentService.students$.pipe(
       map((response) => {
+        this.dataSubject.next(response);
         return { dataState: DataState.LOADED_STATE, appData: response };
       }),
-      startWith({dataState: DataState.LOADING_STATE}),
-      catchError((error: string)=>{
-        return of({dataState: DataState.ERROR_STATE,error: error})
+      startWith({ dataState: DataState.LOADING_STATE }),
+      catchError((error: string) => {
+        return of({ dataState: DataState.ERROR_STATE, error: error });
+      })
+    );
+  }
+
+  saveStudent(studentForm: NgForm) {
+    if (studentForm.valid) {
+      const newStudent: Student = studentForm.value as Student;
+  
+      this.appState$ = this.studentService.add_new_student$(newStudent).pipe(
+        map((response) => {
+          const updatedStudents: Student[] = [
+            ...(this.dataSubject.value?.data.students || []),
+            response.data.student!
+          ];
+  
+          this.dataSubject.next({
+            ...response,
+            data: {
+              students: updatedStudents
+            }
+          });
+  
+          document.getElementById('closeModal')?.click();
+          studentForm.resetForm();
+  
+          return { dataState: DataState.LOADED_STATE, appData: this.dataSubject.value! };
+        }),
+        startWith({ dataState: DataState.LOADING_STATE, appData: this.dataSubject.value! }),
+        catchError((error: string) => {
+          return of({ dataState: DataState.ERROR_STATE, error: error });
+        })
+      );
+    }
+  }
+  
+
+  filterStudents(gender: Gender) {
+    const data = this.dataSubject.value || ({ data: {} } as CustomResponse);
+    this.appState$ = this.studentService.filter$(gender, data).pipe(
+      map((response) => {
+        return { dataState: DataState.LOADED_STATE, appData: response };
+      }),
+      startWith({ dataState: DataState.LOADING_STATE, appData: data }),
+      catchError((error: string) => {
+        return of({ dataState: DataState.ERROR_STATE, error: error });
       })
     );
   }
